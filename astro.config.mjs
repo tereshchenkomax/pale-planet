03c334c54c8b3510ae463ebf4745d79c0bd145d4
Max Tereshchenko <max.tereshchenko@innroad.com>
2023-08-21 17:55:11 +0200

    dynamic base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const isDev = import.meta.env.DEV;
+
 // https://astro.build/config
 export default defineConfig({
   // ...
   integrations: [tailwind()],
   site: 'https://tereshchenkomax.github.io',
-  base: '/pale-planet'
+  base: isDev ? '' : '/pale-planet'
 });
